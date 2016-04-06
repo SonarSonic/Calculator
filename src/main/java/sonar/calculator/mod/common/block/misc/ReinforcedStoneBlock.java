d34812c466cc6741f90b851ff7cd235360fb1aa6
SonarSonic <ollielansdell@hotmail.co.uk>
2016-04-06 16:06:00 +0100

    Fixes #82

--- src/main/java/sonar/calculator/mod/common/block/misc/ReinforcedStoneBlock.java
+++ src/main/java/sonar/calculator/mod/common/block/misc/ReinforcedStoneBlock.java
@@ -1,12 +1,11 @@
 package sonar.calculator.mod.common.block.misc;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
-public class ReinforcedStoneBlock extends Block
-{
-  public ReinforcedStoneBlock()
-  {
-    super(net.minecraft.block.material.Material.rock);
-    setHardness(7.0F);
-  }
+public class ReinforcedStoneBlock extends Block {
+	public ReinforcedStoneBlock() {
+		super(Material.rock);
+		setHardness(7.0F);
+	}
 }
